4585f25403d69f51cb0c8e59a4b734bf52721e39
Yash Chovatiya <yashc@Corp.Optym.net>
2022-03-23 17:30:48 +0530

    Worked on router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import React from "react";
 import SimpleTabs from './SimpleTabs';
+import { Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route } from 'react-router-dom';
+import Todo from "./Todo";
+import Filter from './Filter';
 
 function App() {
   return (
-    <div>
-      <SimpleTabs></SimpleTabs>
+    
+      <div>
+      <a href="/todo">TodoList</a>
+      <a href="/filter">Filter</a>
+      {/* <SimpleTabs></SimpleTabs> */}
+      {/* <button onClick={changeTodo}>Todo list</button>
+      <button onClick={changeFilter}>Filter option</button> */}
+      <Switch>
+        <Route path="/todo" component={Todo}></Route>
+        <Route path="/filter" component={Filter}></Route>
+      </Switch>
+
+
     </div>
+   
+    
   );
 }
 
--- src/components/useDebounce.js
+++ src/components/useDebounce.js
@@ -15,6 +15,7 @@ export default function useDebounce(value, delay) {
       return () => {
         clearTimeout(handler);
       };
+      
     },
     
     [value] 
